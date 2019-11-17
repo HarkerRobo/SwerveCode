fbcd87295b075baaa22f1650474d61a2e46fb4e1
20JatinK <20jatink@students.harker.org>
2019-11-16 16:51:36 -0800

    Start Work on Motion Profile Command
    Co-authored-by: 22Shahzebl <22shahzebl@students.harker.org>
    Co-authored-by: Chirag Kaushik <22chiragk@students.harker.org>
    Co-authored-by: Angela Jia <22angiej@students.harker.org>

--- src/main/java/frc/robot/commands/SwerveDriveWithMotionProfile.java
+++ src/main/java/frc/robot/commands/SwerveDriveWithMotionProfile.java
@@ -1,5 +1,14 @@
 package frc.robot.commands;
+
+import com.ctre.phoenix.motion.BufferedTrajectoryPointStream;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.RobotMap;
 import frc.robot.subsystems.Drivetrain;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.modifiers.SwerveModifier;
 
 /**
  * Follows the specified trajectories by close looping and adding necessary Feed Forwards.
@@ -9,75 +18,28 @@ import frc.robot.subsystems.Drivetrain;
  */
 public class SwerveDriveWithMotionProfile extends Command {
     private static final int MIN_BUFFERED_POINTS = 4;
-    private double leftLastSetpoint;
-    private double rightLastSetpoint;
-    private BufferedStreamTrajectory stream;
-    
-    
-    public SwerveDriveWithMotionProfile(double time) {
+    private static final SwerveModifier.Mode MODE = SwerveModifier.Mode.SWERVE_DEFAULT;
+    private static final double WHEELBASE_WIDTH = Drivetrain.DT_WIDTH * 0.0254; // in to m
+    private static final double WHEELBASE_DEPTH = Drivetrain.DT_LENGTH * 0.0254; // in to m
+    private Trajectory fl;
+    private Trajectory fr;
+    private Trajectory bl;
+    private Trajectory br;
+
+
+    public SwerveDriveWithMotionProfile(int timeDur, Waypoint[] waypoints) {
         requires(Drivetrain.getInstance());
         
-        Drivetrain.getInstance().applyToAllDrive((driveMotors) -> driveMotors.configMotionProfileTrajectoryPeriod(time));
-        timeTaken = leftPath.length * timeDur;
-        Waypoint[] points = new Waypoint[] {
-            new Waypoint(-4, -1, Pathfinder.d2r(-45)),      // Waypoint @ x=-4, y=-1, exit angle=-45 degrees
-            new Waypoint(-2, -2, 0),                        // Waypoint @ x=-2, y=-2, exit angle=0 radians
-            new Waypoint(0, 0, 0)                           // Waypoint @ x=0, y=0,   exit angle=0 radians
-        };
+        Drivetrain.getInstance().applyToAllDrive((driveMotors) -> driveMotors.configMotionProfileTrajectoryPeriod(timeDur));
+
         Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, 1.7, 2.0, 60.0);
-        Trajectory trajectory = Pathfinder.generate(points, config);
-    }
+        Trajectory trajectory = Pathfinder.generate(waypoints, config);
 
-    /**
-     * public void setupTrajectoryStream(BufferedStreamTrajectory stream, double[][] path) {
-        for(int i = 0; i < path.length; i++) {
-            TrajectoryPoint point = new TrajectoryPoint();
-            point.position = Conversions.convert(PositionUnit.FEET, path[i][0], PositionUnit.ENCODER_UNITS);
-            point.velocity = Conversions.convertSpeed(SpeedUnit.FEET_PER_SECOND, path[i][1], SpeedUnit.ENCODER_UNITS);
-            point.arbFeedFwd = path[i][2]*Drivetrain.MOTION_PROF_DRIVE_kA+ Math.signum(point.velocity) * Drivetrain.MOTION_PROF_DRIVE_kS);
-            if(i == 0) 
-                point.zeroPos = true;
-            else if(i == path.length-1) {
-                point.isLastPoint = true;
-                if (isLeft)
-                    leftLastSetpoint = point.position;
-                else
-                    rightLastSetpoint = point.position;
-            }
-            point.profileSlotSelect0 = Drivetrain.MOTION_PROF_SLOT;
-            point.timeDur = 0;
-            stream.Write(point);
-        }
-    }
-    
-    */
-
-    @Override
-    private void initialize() {
-        for (int i = 0; i < trajectory.length(); i++) {
-            Trajectory.Segment seg = trajectory.get(i);
-            
-            System.out.printf("%f,%f,%f,%f,%f,%f,%f,%f\n", 
-                seg.dt, seg.x, seg.y, seg.position, seg.velocity, 
-                    seg.acceleration, seg.jerk, seg.heading);
-        }    
-
-        double wheelbase_width = 0.6;
-
-        // The distance between the front and back sides of the wheelbase is 0.5m
-        double wheelbase_depth = 0.5;
-
-        // The swerve mode to generate will be the 'default' mode, where the 
-        // robot will constantly be facing forward and 'sliding' sideways to 
-        // follow a curved path.
-        SwerveModifier.Mode mode = SwerveModifier.Mode.SWERVE_DEFAULT;
-
-        // Create the Modifier Object
         SwerveModifier modifier = new SwerveModifier(trajectory);
 
         // Generate the individual wheel trajectories using the original trajectory
         // as the centre
-        modifier.modify(wheelbase_width, wheelbase_depth, mode);
+        modifier.modify(WHEELBASE_WIDTH, WHEELBASE_DEPTH, MODE);
 
         Trajectory fl = modifier.getFrontLeftTrajectory();       // Get the Front Left wheel
         Trajectory fr = modifier.getFrontRightTrajectory();      // Get the Front Right wheel
@@ -86,8 +48,22 @@ public class SwerveDriveWithMotionProfile extends Command {
     }
 
     @Override
-    private boolean isFinished() {
-        return Drivetrain.getInstance().getTopLeft().isMotionProfileFinished();
+    public void initialize() {
+        // Drivetrain.getInstance().applyToAllAngle((angleMotor) -> angleMotor.selectProfileSlot(Drivetrain.DRIVE_MOTION_PROF_SLOT, RobotMap.PRIMARY_INDEX));
+        // Drivetrain.getInstance().applyToAllDrive((driveMotor) -> driveMotor.selectProfileSlot(Drivetrain.DRIVE_MOTION_PROF_SLOT, RobotMap.PRIMARY_INDEX));
+    
+        
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        // return Drivetrain.getInstance().getTopLeft().isMotionProfileFinished();
+        return Drivetrain.getInstance().getTopLeft().getAngleMotor().isMotionProfileFinished() && Drivetrain.getInstance().getTopLeft().getDriveMotor().isMotionProfileFinished();
     }
 
 
