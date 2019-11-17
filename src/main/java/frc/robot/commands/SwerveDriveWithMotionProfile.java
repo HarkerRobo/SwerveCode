aae606da4b030ed8c53bc35c9b8a139cad7d91dd
22Shahzebl <45473258+22Shahzebl@users.noreply.github.com>
2019-11-16 19:43:32 -0800

    finish the motion profile command

--- src/main/java/frc/robot/commands/SwerveDriveWithMotionProfile.java
+++ src/main/java/frc/robot/commands/SwerveDriveWithMotionProfile.java
@@ -24,10 +24,18 @@ public class SwerveDriveWithMotionProfile extends Command {
     private static final double WHEELBASE_WIDTH = Drivetrain.DT_WIDTH * 0.0254; // in to m
     private static final double WHEELBASE_DEPTH = Drivetrain.DT_LENGTH * 0.0254; // in to m
 
-    private Trajectory fl;
-    private Trajectory fr;
+    private Trajectory tl;
+    private Trajectory tr;
     private Trajectory bl;
     private Trajectory br;
+    private BufferedTrajectoryPointStream tlAngleStream;
+    private BufferedTrajectoryPointStream tlDriveStream;
+    private BufferedTrajectoryPointStream trAngleStream;
+    private BufferedTrajectoryPointStream trDriveStream;
+    private BufferedTrajectoryPointStream blAngleStream;
+    private BufferedTrajectoryPointStream blDriveStream;
+    private BufferedTrajectoryPointStream brAngleStream;
+    private BufferedTrajectoryPointStream brDriveStream;
 
 
     public SwerveDriveWithMotionProfile(int timeDur, Waypoint[] waypoints) {
@@ -44,10 +52,20 @@ public class SwerveDriveWithMotionProfile extends Command {
         // as the centre
         modifier.modify(WHEELBASE_WIDTH, WHEELBASE_DEPTH, MODE);
 
-        Trajectory fl = modifier.getFrontLeftTrajectory();       // Get the Front Left wheel
-        Trajectory fr = modifier.getFrontRightTrajectory();      // Get the Front Right wheel
+        Trajectory tl = modifier.getFrontLeftTrajectory();       // Get the Front Left wheel
+        Trajectory tr = modifier.getFrontRightTrajectory();      // Get the Front Right wheel
         Trajectory bl = modifier.getBackLeftTrajectory();        // Get the Back Left wheel
         Trajectory br = modifier.getBackRightTrajectory();
+
+        tlAngleStream = createAngleStreamFromTrajectory(tl);
+        tlDriveStream = createDriveStreamFromTrajectory(tl);
+        trAngleStream = createAngleStreamFromTrajectory(tr);
+        trDriveStream = createDriveStreamFromTrajectory(tr);
+        blAngleStream = createAngleStreamFromTrajectory(bl);
+        blDriveStream = createDriveStreamFromTrajectory(bl);
+        brAngleStream = createAngleStreamFromTrajectory(br);
+        brDriveStream = createDriveStreamFromTrajectory(br);
+
     }
 
     private static BufferedTrajectoryPointStream createDriveStreamFromTrajectory(Trajectory traj) {
@@ -88,17 +106,43 @@ public class SwerveDriveWithMotionProfile extends Command {
     @Override
     public void initialize() {
         Drivetrain.getInstance().applyToAllAngle((angleMotor) -> angleMotor.selectProfileSlot(Drivetrain.ANGLE_MOTION_PROF_SLOT, RobotMap.PRIMARY_INDEX));
-        Drivetrain.getInstance().applyToAllDrive((driveMotor) -> driveMotor.selectProfileSlot(Drivetrain.DRIVE_MOTION_PROF_SLOT, RobotMap.PRIMARY_INDEX));        
+        Drivetrain.getInstance().applyToAllDrive((driveMotor) -> driveMotor.selectProfileSlot(Drivetrain.DRIVE_MOTION_PROF_SLOT, RobotMap.PRIMARY_INDEX));     
+        
+        Drivetrain.getInstance().getTopLeft().getAngleMotor().startMotionProfile(tlAngleStream, MIN_BUFFERED_PTS, ControlMode.MotionProfile);
+        Drivetrain.getInstance().getTopLeft().getDriveMotor().startMotionProfile(tlDriveStream, MIN_BUFFERED_PTS, ControlMode.MotionProfile);
+        
+        Drivetrain.getInstance().getTopRight().getAngleMotor().startMotionProfile(trAngleStream, MIN_BUFFERED_PTS, ControlMode.MotionProfile);
+        Drivetrain.getInstance().getTopRight().getDriveMotor().startMotionProfile(trDriveStream, MIN_BUFFERED_PTS, ControlMode.MotionProfile);
+        
+        Drivetrain.getInstance().getBackLeft().getAngleMotor().startMotionProfile(blAngleStream, MIN_BUFFERED_PTS, ControlMode.MotionProfile);
+        Drivetrain.getInstance().getBackLeft().getDriveMotor().startMotionProfile(blDriveStream, MIN_BUFFERED_PTS, ControlMode.MotionProfile);
+        
+        Drivetrain.getInstance().getBackRight().getAngleMotor().startMotionProfile(brAngleStream, MIN_BUFFERED_PTS, ControlMode.MotionProfile);
+        Drivetrain.getInstance().getBackRight().getDriveMotor().startMotionProfile(brDriveStream, MIN_BUFFERED_PTS, ControlMode.MotionProfile);
+   
     }
 
     @Override
     public void execute() {
+        SmartDashboard.putNumber("Top Left Velocity", Drivetrain.getInstance().getTopLeft().getDriveMotor().getActiveTrajectoryVelocity());
+        SmartDashboard.putNumber("Top Left Angle Error", Drivetrain.getInstance().getTopLeft().getAngleMotor().getClosedLoopError());
+        SmartDashboard.putNumber("Top Left Drive Error", Drivetrain.getInstance().getTopLeft().getDriveMotor().getClosedLoopError());
         
+        SmartDashboard.putNumber("Top Right Velocity", Drivetrain.getInstance().getTopRight().getDriveMotor().getActiveTrajectoryVelocity());
+        SmartDashboard.putNumber("Top Right Angle Error", Drivetrain.getInstance().getTopRight().getAngleMotor().getClosedLoopError());
+        SmartDashboard.putNumber("Top Right Drive Error", Drivetrain.getInstance().getTopRight().getDriveMotor().getClosedLoopError());
+
+        SmartDashboard.putNumber("Back Left Velocity", Drivetrain.getInstance().getBackLeft().getDriveMotor().getActiveTrajectoryVelocity());
+        SmartDashboard.putNumber("Back Left Angle Error", Drivetrain.getInstance().getBackLeft().getAngleMotor().getClosedLoopError());
+        SmartDashboard.putNumber("Back Left Drive Error", Drivetrain.getInstance().getBackLeft().getDriveMotor().getClosedLoopError());
+
+        SmartDashboard.putNumber("Back Right Velocity", Drivetrain.getInstance().getBackRight().getDriveMotor().getActiveTrajectoryVelocity());
+        SmartDashboard.putNumber("Back Right Angle Error", Drivetrain.getInstance().getBackRight().getAngleMotor().getClosedLoopError());
+        SmartDashboard.putNumber("Back Right Drive Error", Drivetrain.getInstance().getBackRight().getDriveMotor().getClosedLoopError());
     }
 
     @Override
     public boolean isFinished() {
-        // return Drivetrain.getInstance().getTopLeft().isMotionProfileFinished();
         return Drivetrain.getInstance().getTopLeft().getAngleMotor().isMotionProfileFinished() && Drivetrain.getInstance().getTopLeft().getDriveMotor().isMotionProfileFinished();
     }
 
